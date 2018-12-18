f6af76403b62da003a7746689acf0bec24b12028
Cayden <caydenlund@gmail.com>
2018-12-18 00:47:29 -0700

    Create basis for the main view screen

--- imports/ui/pages/mainViewScreen/mainViewScreen.js
+++ imports/ui/pages/mainViewScreen/mainViewScreen.js
@@ -1,11 +1,73 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
+import {withTracker} from 'meteor/react-meteor-data';
+import {Session} from "meteor/session";
+// Import API
+import Constants from "../../../api/constants/constants";
+// Import screens
+import SplashMVS from "./screens/splashMVS";
+// Import components
+import Line from "../../components/line/line";
 
-export default class MainViewScreen extends Component {
+import "./mainViewScreen.scss";
+
+class MainViewScreen extends Component {
+    getScreen() {
+        let renderedScreen = <SplashMVS/>;
+        switch (this.props.screen) {
+            default:
+                Session.set("screen", null);
+                break;
+        }
+        return renderedScreen;
+    }
+
+    getTransparent(screen) {
+        return (screen === "Radar");
+    }
+
     render() {
         return (
-            <div id={"mainViewScreen"}>
-                <h1 className={"centered"}>Main View Screen</h1>
+            <div id={"MainViewScreen"}>
+                <MainViewScreenHeader screen={this.props.screen} transparent={this.getTransparent(this.props.screen)}/>
+                <div id={"screen"}>
+                    {this.getScreen()}
+                </div>
             </div>
         );
     }
 }
+
+class MainViewScreenHeader extends Component {
+    render() {
+        // noinspection JSUnresolvedVariable
+        return (
+            <div id={"MainViewScreenHeader"} className={(this.props.transparent) ? "transparent" : ""}>
+                <div className={"outer left"}>
+                    <div className={"parallelogram"}>
+                    </div>
+                    <Line/>
+                </div>
+                <div className={"outer right"}>
+                    <div className={"parallelogram"}>
+                    </div>
+                    <Line/>
+                </div>
+                <div className={"inner"}>
+                    <div className={"ship"}>
+                        {Constants.shipName}
+                    </div>
+                    <div className={"right screen"}>
+                        {this.props.screen}
+                    </div>
+                    <Line/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default withTracker(() => {
+    return {
+        screen: Session.get("screen")
+    }
+})(MainViewScreen);
---
+++ imports/ui/pages/mainViewScreen/screens/splashMVS.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+import "./splashMVS.scss";
+
+export default class SplashMVS extends Component {
+    render() {
+        return (
+            <div id={"SplashMVS"} className={"screen"}>
+                <div className={"image"}/>
+            </div>
+        );
+    }
+}
