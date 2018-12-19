85cd48a5e929871edcd5fdd1d6ee756ec3a33e9e
Cayden <caydenlund@gmail.com>
2018-12-19 15:41:51 -0700

    Disable the right-click context menu

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -2,3 +2,8 @@ import "./socket";
 import "./routes";
 
 import "../../ui/styles/main.scss";
+
+// Disable the context menu globally
+document.addEventListener("contextmenu", (event) => {
+    event.preventDefault();
+});
