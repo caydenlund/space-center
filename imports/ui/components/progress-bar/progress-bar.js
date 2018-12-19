aa2c4163dfc3d72a0005f116210ef6d75e652a1f
Cayden <caydenlund@gmail.com>
2018-12-19 15:19:04 -0700

    Create ProgressBar component

---
+++ imports/ui/components/progress-bar/progress-bar.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react";
+import classnames from "classnames";
+
+import "./progress-bar.scss";
+
+export default class ProgressBar extends Component {
+    innerBar() {
+        let classes = ["inner"];
+        classes.push(this.props.color);
+        const className = classnames(classes);
+        return (
+            <div className={className} style={{width: this.props.width + "%"}}>{this.props.children}</div>
+        )
+    }
+
+    render() {
+        let classes = ["progressBar"];
+        classes.push(this.props.className);
+        const className = classnames(classes);
+        return (
+            <div className={className}>
+                {this.innerBar()}
+            </div>
+        )
+    }
+}
