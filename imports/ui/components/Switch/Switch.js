3febfe86b605fb62969eee30481b760304821647
Cayden <caydenlund@gmail.com>
2019-04-04 16:44:12 -0600

    Upgrade npm and meteor packages

---
+++ imports/ui/components/Switch/Switch.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import classnames from "classnames";
+
+import "./Switch.scss";
+
+export default class Switch extends Component {
+    render() {
+        let classes = ["switch-container"];
+        classes.push(this.props.className);
+        if (this.props.enabled === true)
+            classes.push("switch-enabled");
+        let switchClassName = classnames(classes);
+        return (
+            <div className={switchClassName} id={this.props.id} onClick={this.props.onClick}>
+                <div className={"switch-inner"}>
+                </div>
+            </div>
+        );
+    }
+}
